9de7740d0866322b6a1fe1e85243254d7a9386db
GreyMW <GreyMW@gmail.com>
2022-11-16 21:38:58 -0600

    Ship rotation implemented, yet to connect it to keyboard input

--- Engine/Ship.cpp
+++ Engine/Ship.cpp
@@ -1,5 +1,8 @@
 #include "Ship.h"
-
+#include "Vector2.h"
+#include "Matrix_2x2.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 
 Ship::Ship(int center_x, int center_y, int height_in, int width_in)
@@ -8,11 +11,12 @@ Ship::Ship(int center_x, int center_y, int height_in, int width_in)
 	y = center_y;
 	height = height_in;
 	width = width_in;
+	rotation = 0;
 }
 
-void Ship::draw(Graphics& gfx) const
+void Ship::draw(Graphics& gfx) 
 {
-	using std::abs;
+
 	float percentage_x = 1.0f;
 	float percentage_y = 1.0f;
 	for(int j = height; j > ( -1 * height); j--)
@@ -23,12 +27,42 @@ void Ship::draw(Graphics& gfx) const
 
 		for(int i = (-1*width); i < width; i++)
 		{
+			//how far along we are on the triangle
 			percentage_x = float(width - i) / float(width*2);
+
+			//as long as how far along we are is greater or equal to how high we are on the triangle
+			//it's part of the triangle
 			if(abs(percentage_x) >= abs(percentage_y))
 			{
-				gfx.PutPixel(i + x, j + y, 255, 255, 255);	
+
+				Vector2 vec(static_cast<float>(i), static_cast<float>(j));
+
+				vec = rotate_ship(270, vec);
+
+				gfx.PutPixel(vec, 255, 255, 255);
 			}
 			
 		}
 	}
+}
+
+Vector2 Ship::rotate_ship(const float degrees, Vector2& vec)
+{
+	//affine transformation for rotation C = M(B-A)+A
+	//where C is the final vector
+	//B is vec
+	//A is [x, y] or the center of our ship
+	//M is the rotation matrix:
+	//[cos(a) -sin(a)
+	// sin(a) cos(a)]
+
+	const float radians = float(degrees * M_PI / 180.0f);
+
+	const Matrix_2x2 rotation_matrix = Matrix_2x2(float(cos(radians)), float(sin(radians)), -1.0f * float(sin(radians)), float(cos(radians)));
+
+	//the vector comes in as B-A
+	//M(B-A)+A
+	return (rotation_matrix * vec) + Vector2(float(x),float(y));
+	
+
 }
--- Engine/Ship.h
+++ Engine/Ship.h
@@ -1,14 +1,14 @@
 #pragma once
 #include "Graphics.h"
-#include "Vector2.h"
+//#include "Vector2.h"
 
 class Ship
 {
 public:
 	Ship(int center_x, int center_y, int height, int width);
 	//the ship will be drawn as a triangle pointed sideways centered on x and y modified by the rotation
-	void draw(Graphics& gfx) const;
-	void rotate_ship(float degrees);
+	void draw(Graphics& gfx);
+	Vector2 rotate_ship(float degrees, Vector2& vec);
 
 private:
 	
@@ -16,5 +16,6 @@ private:
 	int y;
 	int width;
 	int height;
+	int rotation;
 
 };
